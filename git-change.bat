1c5678f30d9bd39311368065efe47f32a344c252
FGI <gumuruh@fgroupindonesia.com>
2023-06-05 17:32:03 +0700

    first commit

--- git-change.bat
+++ git-change.bat
@@ -1,5 +1,6 @@
 git remote -v
-git remote set-url origin git@github.com:fgroupindonesia/sistemantrian.git
+git remote set-url git@github.com:fgroupindonesia/sistemantrian.git
+git push
 git add .
 git commit -m "first commit"
 git push -u origin main
